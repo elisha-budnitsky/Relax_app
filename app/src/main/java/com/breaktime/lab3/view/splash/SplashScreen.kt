23903ba2588faa8e21c4914eed6a2f48ea2b342e
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-05 18:13:26 +0300

    feat: add splash screen

--- app/src/main/java/com/breaktime/lab3/view/splash/SplashScreen.kt
+++ app/src/main/java/com/breaktime/lab3/view/splash/SplashScreen.kt
@@ -1,9 +1,57 @@
 package com.breaktime.lab3.view.splash
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import com.breaktime.lab3.R
+import com.breaktime.lab3.navigation.Screen
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1500)
+    )
 
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(2000)
+        navController.popBackStack()
+        navController.navigate(Screen.Onboarding.route)
+    }
+    Splash(alpha = alphaAnim.value)
+}
+
+@Composable
+private fun Splash(alpha: Float) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentDescription = "background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+
+        Image(
+            painter = painterResource(R.drawable.ic_logo),
+            contentDescription = "logo",
+            alignment = Alignment.Center,
+            contentScale = ContentScale.None,
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(alpha = alpha)
+        )
+    }
 }
