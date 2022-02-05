9249ad7c90f4dc53abdd456da4e66c3611f909f1
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-05 19:13:51 +0300

    feat: add onboard ui

--- app/src/main/java/com/breaktime/lab3/view/onboarding/OnboardingScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.breaktime.lab3.view.onboarding
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-
-@Composable
-fun OnboardingScreen(navController: NavHostController) {
-
-}
--- app/src/main/java/com/breaktime/lab3/view/splash/SplashScreen.kt
+++ app/src/main/java/com/breaktime/lab3/view/splash/SplashScreen.kt
@@ -3,14 +3,20 @@ package com.breaktime.lab3.view.splash
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.breaktime.lab3.R
 import com.breaktime.lab3.navigation.Screen
@@ -18,19 +24,34 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    var startAnimation by remember { mutableStateOf(false) }
-    val alphaAnim = animateFloatAsState(
-        targetValue = if (startAnimation) 1f else 0f,
+    var startLogoAnimation by remember { mutableStateOf(false) }
+    val alphaLogoAnim = animateFloatAsState(
+        targetValue = if (startLogoAnimation) 1f else 0f,
         animationSpec = tween(durationMillis = 1500)
+    )
+    var startOnboardAnimation by remember { mutableStateOf(false) }
+    val alphaOnboardAnim = animateFloatAsState(
+        targetValue = if (startOnboardAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
     )
 
     LaunchedEffect(key1 = true) {
-        startAnimation = true
+        startLogoAnimation = true
         delay(2000)
-        navController.popBackStack()
-        navController.navigate(Screen.Onboarding.route)
+        startOnboardAnimation = true
     }
-    Splash(alpha = alphaAnim.value)
+    Splash(alpha = alphaLogoAnim.value)
+    Onboarding(
+        alpha = alphaOnboardAnim.value,
+        onClickLogin = {
+            navController.popBackStack()
+            navController.navigate(Screen.Login.route)
+        },
+        onClickRegister = {
+            navController.popBackStack()
+            navController.navigate(Screen.Registration.route)
+        }
+    )
 }
 
 @Composable
@@ -47,11 +68,92 @@ private fun Splash(alpha: Float) {
         Image(
             painter = painterResource(R.drawable.ic_logo),
             contentDescription = "logo",
-            alignment = Alignment.Center,
+            alignment = Alignment.TopCenter,
             contentScale = ContentScale.None,
             modifier = Modifier
                 .fillMaxSize()
                 .alpha(alpha = alpha)
         )
+    }
+}
+
+@Composable
+private fun Onboarding(
+    alpha: Float,
+    onClickRegister: () -> Unit,
+    onClickLogin: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(alpha = alpha),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 60.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Привет",
+                color = Color.White, fontSize = 36.sp
+            )
+            Text(
+                text = "Наслаждайся отборочным.",
+                color = Color.White, fontSize = 22.sp
+            )
+            Text(
+                text = "Будет внимателен.",
+                color = Color.White, fontSize = 22.sp
+            )
+            Text(
+                text = "Делай хорошо.",
+                color = Color.White, fontSize = 22.sp
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 100.dp)
+        ) {
+            Button(
+                modifier = Modifier
+                    .padding(25.dp)
+                    .fillMaxWidth(),
+                onClick = onClickLogin
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp),
+                    text = "Войти в аккаунт",
+                    color = Color.White,
+                    fontSize = 22.sp
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Еще нет аккаунта?",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                TextButton(
+                    onClick = onClickRegister
+                )
+                {
+                    Text(
+                        text = "Зарегистрируйтесь",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                    )
+                }
+            }
+        }
     }
 }
