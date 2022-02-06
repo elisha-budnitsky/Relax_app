32e9959f7b09896a0110d43cace2da6fb39f0fbd
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-06 11:39:20 +0300

    fix: remove onboarding screen

--- app/src/main/java/com/breaktime/lab3/navigation/Screen.kt
+++ app/src/main/java/com/breaktime/lab3/navigation/Screen.kt
@@ -1,13 +1,12 @@
 package com.breaktime.lab3.navigation
 
 sealed class Screen(val route: String) {
-    object Splash: Screen(route = "splash_screen")
-    object Onboarding: Screen(route = "onboarding_screen")
-    object Login: Screen(route = "login_screen")
-    object Registration: Screen(route = "registration_screen")
-    object Main: Screen(route = "main_screen")
-    object Menu: Screen(route = "menu_screen")
-    object Photo: Screen(route = "photo_screen")
-    object Listen: Screen(route = "listen_screen")
-    object Profile: Screen(route = "profile_screen")
+    object Splash : Screen(route = "splash_screen")
+    object Login : Screen(route = "login_screen")
+    object Registration : Screen(route = "registration_screen")
+    object Main : Screen(route = "main_screen")
+    object Menu : Screen(route = "menu_screen")
+    object Photo : Screen(route = "photo_screen")
+    object Listen : Screen(route = "listen_screen")
+    object Profile : Screen(route = "profile_screen")
 }
