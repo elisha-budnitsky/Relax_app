5393096a0c616eedd8f3c4cfec3c5e9a3164ad1e
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-09 00:54:11 +0300

    feat: add first enter screen

---
+++ app/src/main/java/com/breaktime/lab3/view/first_enter/FirstEnterScreen.kt
@@ -0,0 +1,185 @@
+package com.breaktime.lab3.view.first_enter
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.breaktime.lab3.R
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+// TODO: come from sing up or if user don't have saved info
+@Composable
+fun FirstEnterScreen() {
+    val phoneNumber = remember { mutableStateOf("") }
+    val weight = remember { mutableStateOf("") }
+    val pressure = remember { mutableStateOf("") }
+    val birthday = remember { mutableStateOf("") }
+    val phoneNumberData =
+        ScreenData(
+            "Enter your phone number",
+            R.drawable.phone,
+            "phone number",
+            "next",
+            phoneNumber,
+            ::checkNumber
+        )
+    val weightData =
+        ScreenData(
+            "Enter your weight",
+            R.drawable.weight,
+            "weight",
+            "next",
+            weight,
+            ::check3Number
+        )
+    val pressureData =
+        ScreenData(
+            "Enter your pressure",
+            R.drawable.weight,
+            "pressure",
+            "next",
+            pressure,
+            ::check3Number
+        )
+    val birthdayData =
+        ScreenData(
+            "Enter your birthday date",
+            R.drawable.calendar,
+            "08.03.1998",
+            "finish",
+            pressure,
+            ::checkBirthday
+        )
+
+    when {
+        phoneNumber.value.isEmpty() -> {
+            GetDataScreen(screenData = phoneNumberData)
+        }
+        weight.value.isEmpty() -> {
+            GetDataScreen(screenData = weightData)
+        }
+        pressure.value.isEmpty() -> {
+            GetDataScreen(screenData = pressureData)
+        }
+        birthday.value.isEmpty() -> {
+            GetDataScreen(screenData = birthdayData)
+        }
+        else -> {
+            println("finish")
+            // TODO: save data
+            // TODO: move to home
+        }
+    }
+}
+
+fun checkBirthday(text: String): Boolean {
+    return try {
+        val df = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        df.isLenient = false
+        df.parse(text)
+        true
+    } catch (e: ParseException) {
+        false
+    }
+}
+
+fun checkNumber(text: String): Boolean {
+    return text.matches(Regex("[0-9]+"))
+}
+
+fun check3Number(text: String): Boolean {
+    return text.matches(Regex("[0-9]{1,3}"))
+}
+
+@Composable
+private fun GetDataScreen(screenData: ScreenData) {
+    val context = LocalContext.current
+    var text by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF283234)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text(
+                text = screenData.title,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+            Image(
+                painter = painterResource(screenData.image),
+                contentDescription = "avatar",
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(vertical = 40.dp)
+                    .height(150.dp)
+                    .fillMaxWidth()
+            )
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 40.dp)
+                    .padding(horizontal = 25.dp),
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(screenData.fieldLabel, color = MaterialTheme.colors.primary) },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    textColor = Color.White,
+                    cursorColor = Color.White
+                ),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 25.dp)
+                    .padding(top = 40.dp)
+                    .alpha(0.8f),
+                onClick = {
+                    if (screenData.check(text))
+                        screenData.returnValue.value = text
+                    else
+                        Toast.makeText(context, "Check data", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp),
+                    text = screenData.buttonText,
+                    color = Color.White,
+                    fontSize = 22.sp
+                )
+            }
+        }
+    }
+}
+
+private data class ScreenData(
+    val title: String,
+    val image: Int,
+    val fieldLabel: String,
+    val buttonText: String,
+    val returnValue: MutableState<String>,
+    val check: (String) -> Boolean
+)
