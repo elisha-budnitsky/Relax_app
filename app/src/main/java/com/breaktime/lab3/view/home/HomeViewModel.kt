6909a4ed558c92edf13857168957e55fe0bb346f
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-09 18:25:29 +0300

    feat: add horoscope by birthday

--- app/src/main/java/com/breaktime/lab3/view/home/HomeViewModel.kt
+++ app/src/main/java/com/breaktime/lab3/view/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.breaktime.lab3.view.home
 
 import androidx.lifecycle.viewModelScope
 import com.breaktime.lab3.R
+import com.breaktime.lab3.data.User
 import com.breaktime.lab3.data.dailySuggestions
 import com.breaktime.lab3.data.todaySuggestions
 import com.breaktime.lab3.firebase.Firebase
@@ -15,6 +16,9 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class HomeViewModel(private val firebase: Firebase) :
     BaseViewModel<HomeContract.Event, HomeContract.State, HomeContract.Effect>() {
@@ -61,9 +65,10 @@ class HomeViewModel(private val firebase: Firebase) :
 
     private fun getHoroscopeData() {
         viewModelScope.launch(Dispatchers.IO) {
+            val sign = detectSign()
             val client = OkHttpClient()
             val request = Request.Builder()
-                .url("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today")
+                .url("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=$sign&day=today")
                 .post(RequestBody.create(null, ByteArray(0)))
                 .addHeader("x-rapidapi-host", "sameer-kumar-aztro-v1.p.rapidapi.com")
                 .addHeader(
@@ -83,6 +88,28 @@ class HomeViewModel(private val firebase: Firebase) :
                 }
             }
         }
+    }
+
+    private fun detectSign(): String {
+        val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        val date = sdf.parse(User.birthday)
+        val cal: Calendar = Calendar.getInstance()
+        cal.time = date
+        val day = cal.get(Calendar.DATE)
+        val month = cal.get(Calendar.MONTH) + 1
+        return if (month == 12 && day >= 22 && day <= 31 || month == 1 && day >= 1 && day <= 19) "capricorn"
+        else if (month == 1 && day >= 20 && day <= 31 || month == 2 && day >= 1 && day <= 17) "aquarius"
+        else if (month == 2 && day >= 18 && day <= 29 || month == 3 && day >= 1 && day <= 19) "pisces"
+        else if (month == 3 && day >= 20 && day <= 31 || month == 4 && day >= 1 && day <= 19) "aries"
+        else if (month == 4 && day >= 20 && day <= 30 || month == 5 && day >= 1 && day <= 20) "taurus"
+        else if (month == 5 && day >= 21 && day <= 31 || month == 6 && day >= 1 && day <= 20) "gemini"
+        else if (month == 6 && day >= 21 && day <= 30 || month == 7 && day >= 1 && day <= 22) "cancer"
+        else if (month == 7 && day >= 23 && day <= 31 || month == 8 && day >= 1 && day <= 22) "leo"
+        else if (month == 8 && day >= 23 && day <= 31 || month == 9 && day >= 1 && day <= 22) "virgo"
+        else if (month == 9 && day >= 23 && day <= 30 || month == 10 && day >= 1 && day <= 22) "libra"
+        else if (month == 10 && day >= 23 && day <= 31 || month == 11 && day >= 1 && day <= 21) "scorpio"
+        else if (month == 11 && day >= 22 && day <= 30 || month == 12 && day >= 1 && day <= 21) "sagittarius"
+        else "Illegal date"
     }
 
     private fun buildDailyData(ofterMood: Mood?): MoodData {
